df60292eb35f2847f04253eeedfcfaf96ef08e2d
Hanserprpr <2041283903@qq.com>
2025-07-08 16:55:26 +0800

    Initial commit

---
+++ src/main/java/com/orbithy/sduonline_simulation/SduonlineSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.orbithy.sduonline_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SduonlineSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SduonlineSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orbithy/sduonline_simulation/SduonlineSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbithy.sduonline_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SduonlineSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
